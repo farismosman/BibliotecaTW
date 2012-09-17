41c0b8c0f961bee2cdcfa227752c37d2e3a3c5d9
Faris Mohammed <farismosman@gmail.com>
2012-09-17 11:15:24 +0200

    23- Refactored Library by removing unused code.. The constants in AppMenu are in uppercase now.

--- src/AppMenu.java
+++ src/AppMenu.java
@@ -1,11 +1,11 @@
 public class AppMenu {
-    private static final String viewBooks = "To view all books type \"view books\"\n";
-    private static final String reserveBook = "To reserve a book type \"reserve book\"\n";
-    private static final String libraryNumber = "To check your library number type \"library number\"\n"; 
-    public static final String quitApp = "To quit application type \"q\" or \"Q\"";
+    private static final String VIEW_BOOKS = "To view all books type \"view books\"\n";
+    private static final String RESERVE_BOOK = "To reserve a book type \"reserve book\"\n";
+    private static final String LIBRARY_NUMBER = "To check your library number type \"library number\"\n"; 
+    private static final String QUIT_APP = "To quit application type \"q\" or \"Q\"";
     
     public String userOptions() {
-        return viewBooks + reserveBook + libraryNumber + quitApp;
+        return VIEW_BOOKS + RESERVE_BOOK + LIBRARY_NUMBER + QUIT_APP;
     }
 
 }
--- src/Library.java
+++ src/Library.java
@@ -2,13 +2,6 @@ import java.util.HashMap;
 
 public class Library {
 
-    private Biblioteca biblioteca;
-
-    public Library(Biblioteca biblioteca) {
-        this.biblioteca = biblioteca;
-        books();
-    }
-
     private HashMap<String, String> allBooks = new HashMap<String, String>();
 
     protected final HashMap<String, String> books() {
